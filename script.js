f1642de6036d3514c03c2150bad0906e1c16008e
Alishba <a.alishba1002@gmail.com>
2025-10-06 14:40:27 +0500

    refactor: enhance navigation and add hover effects

---
+++ script.js
@@ -0,0 +1,8 @@
+let navItem = document.querySelector(".nav-item")
+
+navItem.addEventListener("click", ()=>{
+    alert('Dashboard clicked! This would navigate to dashboard page.');
+
+    this.style.background = '#e6f0ff';
+    this.style.color = '#0066ff';
+});
