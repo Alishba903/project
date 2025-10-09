35cf26a1a265cab9e837d9fe1a8233aa03607710
Alishba <a.alishba1002@gmail.com>
2025-10-09 17:37:54 +0500

    Add styles for main content

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let navItem = document.querySelector(".nav-item")
 
 navItem.addEventListener("click", ()=>{
-    alert('Dashboard clicked! This would navigate to dashboard page.');
+    // alert('Dashboard clicked! This would navigate to dashboard page.');
 
     this.style.background = '#e6f0ff';
     this.style.color = '#0066ff';
